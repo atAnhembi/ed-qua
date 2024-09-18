66f56e771542b394e7a08f21b1445d087f866d7b
EmersonPaduan <emerson.paduan@gmail.com>
2024-09-18 20:31:00 -0300

    aula 06 - parte 1

---
+++ aula06/MyList.java
@@ -0,0 +1,15 @@
+
+public class MyList<T> {
+    private Node<T> first, last;
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+    
+    public void add(T data){
+        Node<T> newNode = new Node<>(data);
+
+        
+    }
+
+}
---
+++ aula06/Node.java
@@ -0,0 +1,29 @@
+/**
+ * Node
+ */
+public class Node<T> {
+    private T data;
+    Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        next = null;
+    }
+
+    public Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
