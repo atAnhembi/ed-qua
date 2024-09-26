52cb69b3bb8b97b1b2a533d7c40021ed01481951
EmersonPaduan <emerson.paduan@gmail.com>
2024-09-25 21:41:01 -0300

    aula07 - exercicios

--- aula07/DoubleList.java
+++ aula07/DoubleList.java
@@ -1,15 +1,22 @@
 public class DoubleList<T> {
-    private Node<T> fisrt, last;
+    private Node<T> first, last;
+    private int size;
 
     public boolean isEmpty() {
-        return fisrt == null;
+        return first == null;
+    }
+
+    public int size() {
+        return size;
     }
 
     public void insert(T data) {
         Node<T> newNode = new Node<T>(data);
 
+        size++;
+
         if(isEmpty()) {
-            fisrt = last = newNode;
+            first = last = newNode;
             return;
         }
 
@@ -23,12 +30,61 @@ public class DoubleList<T> {
         if(isEmpty()) {
             return "Empty";
         }
-        String resp = fisrt.getData().toString();
-        Node<T> temp = fisrt.getNext();
+        String resp = first.getData().toString();
+        Node<T> temp = first.getNext();
         while (temp != null) {
             resp += ";" + temp.getData();
             temp = temp.getNext();
         }
         return resp;
+    }
+
+    public T removeLast() {
+        if(isEmpty()) {
+            return null;
+        }
+        T data = last.getData();
+        last = last.getPrev();
+
+        if(last == null) {
+            first = null;
+        } else {
+            last.setNext(null);
+        }
+        size--;
+        return data;
+    }
+
+    /* Exercícios  */
+
+     // retorna o valor que está em uma certa posição, sem remover
+     // OBS: Validar a posição
+     public T get(int position) {
+        return null;
+     }
+     // Verifica se um dado está presente na lista
+    public boolean exists(T data) {
+        return false;
+    }
+
+    // adiciona um dado em uma posição específica
+    // precisa validar a posição
+    public void add(T data, int position) {
+
+    }
+
+    // remove um data caso ele exista na lista
+    public boolean remove(T data) {
+        return false;
+    }
+
+    // remove o dado que estiver em uma determinada posição
+    public T remove(int position) {
+        return null;
+    }
+
+    // altera o dado que está em uma posição
+    public void set(T data, int position) {
+
     }
 }
--- aula07/Main.java
+++ aula07/Main.java
@@ -6,6 +6,11 @@ public class Main {
         lista.insert("BMW");
         lista.insert("Ferrari");
         
-        System.out.println(lista);
+        System.out.println(lista + " com tamanho " +lista.size());
+        
+        while (!lista.isEmpty()) {
+            System.out.println("Removeu: " + lista.removeLast());
+            System.out.println(lista + " com tamanho " +lista.size());
+        }
     }
 }
